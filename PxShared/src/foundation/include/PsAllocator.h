d11a8a34882c0e380cc0df2145d07d0beb627681
pkusilence <pkusilence@gmail.com>
2021-12-05 15:49:24 +0800

    Update compile settings for VS2019

--- PxShared/src/foundation/include/PsAllocator.h
+++ PxShared/src/foundation/include/PsAllocator.h
@@ -1,4 +1,4 @@
-//
+﻿//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
 // are met:
@@ -37,7 +37,7 @@
 
 #if(PX_WINDOWS_FAMILY || PX_XBOXONE)
 #include <exception>
-#include <typeinfo.h>
+#include <typeinfo>
 #endif
 #if(PX_APPLE_FAMILY)
 #include <typeinfo>
